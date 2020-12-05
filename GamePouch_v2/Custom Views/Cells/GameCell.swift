5de3d080bdeab8d38c5d35fd78efb8f69e0aa8d6
janicelee <j382lee@gmail.com>
2020-12-04 19:45:01 -0500

    Create table view on search screen

---
+++ GamePouch_v2/Custom Views/Cells/GameCell.swift
@@ -0,0 +1,41 @@
+//
+//  GameCell.swift
+//  GamePouch_v2
+//
+//  Created by Janice Lee on 2020-12-03.
+//
+
+import UIKit
+
+class GameCell: UITableViewCell {
+    static let reuseID = "GameCell"
+    
+    let label = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        addSubview(label)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .systemRed
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: self.topAnchor),
+            label.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            label.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            label.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+    
+    func set(game: Game) {
+        label.text = game.id
+    }
+}
--- GamePouch_v2/ViewControllers/SearchViewController.swift
+++ GamePouch_v2/ViewControllers/SearchViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    private let tableView = UITableView()
+    
+    private var games: [Game] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
+        configureTableView()
+        getHotnessList()
     }
     
     private func configureViewController() {
@@ -19,5 +25,42 @@ class SearchViewController: UIViewController {
         title = "Hot Games"
         navigationController?.navigationBar.prefersLargeTitles = true
     }
+    
+    private func configureTableView() {
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(GameCell.self, forCellReuseIdentifier: GameCell.reuseID)
+    }
+    
+    private func getHotnessList() {
+        NetworkManager.shared.getHotnessList { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let games):
+                self.games = games
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            case .failure(let error):
+                print(error.rawValue)
+            }
+        }
+    }
 }
 
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return games.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: GameCell.reuseID) as! GameCell
+        cell.set(game: games[indexPath.row])
+        return cell
+    }
+}
