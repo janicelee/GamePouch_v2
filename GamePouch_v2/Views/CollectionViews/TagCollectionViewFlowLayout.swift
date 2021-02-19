50c31352e2c6b36ae6b5e69ae8ad3293d7dd9cbc
janicelee <j382lee@gmail.com>
2021-02-18 23:31:28 -0500

    Fix error: layout attributes need to be copied before modifying in TagCollectionViewFlowLayout

--- GamePouch_v2/Views/CollectionViews/TagCollectionViewFlowLayout.swift
+++ GamePouch_v2/Views/CollectionViews/TagCollectionViewFlowLayout.swift
@@ -15,18 +15,22 @@ class TagCollectionViewFlowLayout: UICollectionViewFlowLayout {
     
     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
         let attributes = super.layoutAttributesForElements(in: rect)
-
+        var updatedAttributes: [UICollectionViewLayoutAttributes] = []
         var leftMargin = sectionInset.left
         var maxY: CGFloat = -1.0
+        
         attributes?.forEach { layoutAttribute in
-            if layoutAttribute.frame.origin.y >= maxY {
+            let updatedAttribute = layoutAttribute.copy() as! UICollectionViewLayoutAttributes
+            
+            if updatedAttribute.frame.origin.y >= maxY {
                 leftMargin = sectionInset.left
             }
-            layoutAttribute.frame.origin.x = leftMargin
+            updatedAttribute.frame.origin.x = leftMargin
 
-            leftMargin += layoutAttribute.frame.width + minimumInteritemSpacing
-            maxY = max(layoutAttribute.frame.maxY , maxY)
+            leftMargin += updatedAttribute.frame.width + minimumInteritemSpacing
+            maxY = max(updatedAttribute.frame.maxY , maxY)
+            updatedAttributes.append(updatedAttribute)
         }
-        return attributes
+        return updatedAttributes
     }
 }
